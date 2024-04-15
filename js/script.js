ffeda8ea1e01bacd4c0ae0926280719a9a1886f8
masudulalam <masuudulalam@gmail.com>
2024-04-15 15:22:53 +0600

    update some ui

--- js/script.js
+++ js/script.js
@@ -3,8 +3,27 @@ function removeClassById () {
     element.classList.remove('bg-[#F7F8F8]');
 }
 
+function previousValue () {
+    const element = document.getElementById('seat-numbers');
+    const elementTextValue = element.innerText;
+    const previousValue = parseInt(elementTextValue);
+    // console.log(typeof previousValue, previousValue);
+    return previousValue;
+}
+
+function presentValue () {
+    const currentNumbersOfSeat = previousValue () + 1;
+    // console.log(currentNumbersOfSeat);
+    return currentNumbersOfSeat;
+}
+
+function setUpdateValue () {
+    document.getElementById('seat-numbers').innerText = presentValue ();
+}
+
 function addBackgroundColor () {
     removeClassById ();
     const element = document.getElementById('a1');
     element.classList.add('bg-[#1DD100]', 'text-white');
+    setUpdateValue();
 }
