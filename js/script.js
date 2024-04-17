99b4edc747d3abf90888b9b604217167ad1367c4
masudulalam <masuudulalam@gmail.com>
2024-04-17 23:29:48 +0600

    add change background-color by click event

--- js/script.js
+++ js/script.js
@@ -1,27 +1,14 @@
-// function previousValue() {
-//     const element = document.getElementById('seat-numbers');
-//     const elementTextValue = element.innerText;
-//     const previousValue = parseInt(elementTextValue);
-//     // console.log(typeof previousValue, previousValue);
-//     return previousValue;
-// }
-// // get seat count numbers
-// function presentValue() {
-//     const currentNumbersOfSeat = previousValue() + 1;
-//     // console.log(currentNumbersOfSeat);
-//     return currentNumbersOfSeat;
-// }
-// // set seat count numbers update
-// function setUpdateValue() {
-//     document.getElementById('seat-numbers').innerText = presentValue();
-// }
+// change background color
 
-
-// get inner text
-function getInnerTextById() {
-    const element = document.getElementById('a1');
-    const text = element.innerText;
-    return text;
+function changeColor(element) {
+    element.style.backgroundColor = '#1DD100';
+    element.style.color = 'white';
+}
+let seats = document.getElementsByClassName('seat-btn');
+for (const seat of seats) {
+    seat.addEventListener('click', function () {
+        changeColor(seat);
+    })
 }
 
 // get all seat-button
@@ -72,54 +59,3 @@ for (const button of allSeatBtn) {
 function setInnerText(id, value) {
     document.getElementById(id).innerText = value;
 }
-
-
-
-
-// ---------------------------------------------------------------
-function addDynamicEvent(event) {
-    function changeColor(element) {
-        element.style.backgroundColor = '#1DD100';
-        element.style.color = 'white';
-    }
-    let seats = document.getElementsByClassName('seat');
-    for (const seat of seats) {
-        seat.addEventListener('click', function () {
-            changeColor(this);
-        })
-    }
-
-    // remove background color
-    removeBackgroundColorById();
-    // set background color
-    setBackgroundColorById();
-
-    // set seat count numbers update
-    setUpdateValue();
-
-    // get seat-left value
-    getSeatLeftValue();
-    // set seat-left value
-    setSeatLeftValue();
-
-    // append seat number
-    const element = document.createElement('p');
-    const text = getInnerTextById();
-    element.append(text);
-    document.getElementById('ticket-info').appendChild(element);
-    // append class
-    const classElement = document.createElement('p');
-    classElement.innerText = 'Economy';
-    document.getElementById('ticket-info').appendChild(classElement);
-    // append price
-    const priceElement = document.createElement('p');
-    priceElement.innerText = '550';
-    document.getElementById('ticket-info').appendChild(priceElement);
-
-    // set Total Price
-    setTotalPrice('total-price');
-
-    // set Grand Total
-    setGrandTotal();
-}
-// ---------------------------------------------------------------
