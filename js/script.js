48fdc3cbb3b46e001b70293ae4b7a03ade5a96f3
masudulalam <masuudulalam@gmail.com>
2024-04-17 21:35:43 +0600

    dynamically added seat number, class and price by click event

--- js/script.js
+++ js/script.js
@@ -31,32 +31,52 @@ let count = 0;
 let seatLeft = 40;
 for (const button of allSeatBtn) {
     // add click event all seat-button
-    button.addEventListener('click', function(event){
+    button.addEventListener('click', function (event) {
         // seat count increases 1 by every click event
         count = count + 1;
         // set seat-count numbers by calling function
-        setInnerText ('seat-numbers', count);
+        setInnerText('seat-numbers', count);
 
         // seat-left decreases 1 by every click event
         seatLeft = seatLeft - 1;
         // set seat-left numbers by calling function
-        setInnerText ('total-seats', seatLeft);
+        setInnerText('total-seats', seatLeft);
+
+        // set ticket number
+        const ticketNumber = button.innerText;
+        const paragraphOne = document.createElement('p');
+        paragraphOne.innerText = ticketNumber;
+        // set ticket class
+        const paragraphTwo = document.createElement('p');
+        paragraphTwo.innerText = 'Economy';
+        // set ticket price
+        const paragraphThree = document.createElement('p');
+        paragraphThree.innerText = '550';
+        // display ticket number, class & price 
+        const div = document.createElement('div');
+        div.appendChild(paragraphOne);
+        div.appendChild(paragraphTwo);
+        div.appendChild(paragraphThree);
+        div.setAttribute('id','ticket-info-div')
+        document.getElementById('ticket-info').appendChild(div);
         
-        // const seatNumber = event.target.parentNode.childNodes[21].innerText; 
-        // console.log(event.target.parentNode.childNodes);
+        
     })
-} 
+}
 // for set inner-text by given id & value
-function setInnerText  (id, value) {
+function setInnerText(id, value) {
     document.getElementById(id).innerText = value;
 }
+
+
+
 
 
 function addDynamicEvent(event) {
     function changeColor(element) {
         element.style.backgroundColor = '#1DD100';
         element.style.color = 'white';
-      }
+    }
     let seats = document.getElementsByClassName('seat');
     for (const seat of seats) {
         seat.addEventListener('click', function () {
