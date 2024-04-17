6174fca45a0c0ebce843cee8d3151e7fed6974a0
masudulalam <masuudulalam@gmail.com>
2024-04-17 11:37:13 +0600

    changes select your seat section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-function removeClassById () {
-    const element = document.getElementById('a1');
-    element.classList.remove('bg-[#F7F8F8]');
-}
-
-function previousValue () {
+function previousValue() {
     const element = document.getElementById('seat-numbers');
     const elementTextValue = element.innerText;
     const previousValue = parseInt(elementTextValue);
@@ -11,19 +6,67 @@ function previousValue () {
     return previousValue;
 }
 
-function presentValue () {
-    const currentNumbersOfSeat = previousValue () + 1;
+// get seat count numbers
+function presentValue() {
+    const currentNumbersOfSeat = previousValue() + 1;
     // console.log(currentNumbersOfSeat);
     return currentNumbersOfSeat;
 }
 
-function setUpdateValue () {
-    document.getElementById('seat-numbers').innerText = presentValue ();
+// set seat count numbers update
+function setUpdateValue() {
+    document.getElementById('seat-numbers').innerText = presentValue();
 }
 
-function addBackgroundColor () {
-    removeClassById ();
+// get inner text
+function getInnerTextById() {
     const element = document.getElementById('a1');
-    element.classList.add('bg-[#1DD100]', 'text-white');
+    const text = element.innerText;
+    return text;
+}
+
+function addDynamicEvent(event) {
+    function changeColor(element) {
+        element.style.backgroundColor = '#1DD100';
+        element.style.color = 'white';
+      }
+    let seats = document.getElementsByClassName('seat');
+    for (const seat of seats) {
+        seat.addEventListener('click', function () {
+            changeColor(this);
+        })
+    }
+
+    // remove background color
+    // removeBackgroundColorById();
+    // set background color
+    // setBackgroundColorById();
+
+    // set seat count numbers update
     setUpdateValue();
+
+    // get seat-left value
+    getSeatLeftValue();
+    // set seat-left value
+    setSeatLeftValue();
+
+    // append seat number
+    const element = document.createElement('p');
+    const text = getInnerTextById();
+    element.append(text);
+    document.getElementById('ticket-info').appendChild(element);
+    // append class
+    const classElement = document.createElement('p');
+    classElement.innerText = 'Economy';
+    document.getElementById('ticket-info').appendChild(classElement);
+    // append price
+    const priceElement = document.createElement('p');
+    priceElement.innerText = '550';
+    document.getElementById('ticket-info').appendChild(priceElement);
+
+    // set Total Price
+    setTotalPrice('total-price');
+
+    // set Grand Total
+    setGrandTotal();
 }
---
+++ js/utilities.js
@@ -0,0 +1,59 @@
+// Remove background color by element id
+function removeBackgroundColorById (elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-[#F7F8F8]');
+}
+
+// Set background color by id
+function setBackgroundColorById (elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-[#1DD100]', 'text-white');
+}
+
+// get seat left numbers 
+function getSeatLeftValue () {
+    const element = document.getElementById('total-seats');
+    const elementTextValue = element.innerText;
+    // console.log(typeof elementTextValue, elementTextValue);
+    const value = parseInt(elementTextValue);
+    // console.log(typeof value, value);
+    return value;
+}
+// set seat-left numbers 
+function setSeatLeftValue () {
+    const element = document.getElementById('total-seats');
+    element.innerText = getSeatLeftValue () - 1;
+    // console.log(element);
+}
+
+// previous total price
+function previousTotalPrice () {
+    const element = document.getElementById('total-price');
+    const textValue = element.innerText;
+    const numValue = parseInt(textValue);
+    return numValue;
+}
+
+// set present total price
+function setTotalPrice () {
+    const previousTotal = previousTotalPrice();
+    const presentTotal = previousTotal + 550;
+    const element = document.getElementById('total-price');
+    element.innerText = presentTotal;
+}
+
+// previous grand total price
+function previousGrandTotal () {
+    const element = document.getElementById('grand-total');
+    const textValue = element.innerText;
+    const numValue = parseInt(textValue);
+    return numValue;
+}
+
+// set present grand total price
+function setGrandTotal () {
+    const previousTotal = previousGrandTotal();
+    const presentTotal = previousTotal + 550;
+    const element = document.getElementById('grand-total');
+    element.innerText = presentTotal;
+}
